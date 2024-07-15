d04ec6dd33e5229380dd4bf1d0d5ab2c4387dcf4
Carter Small <melaniesmall461@gmail.com>
2024-07-15 08:36:44 -0600

    File Transfer

--- Assets/Scripts/Camera/Follower.cs
+++ Assets/Scripts/Camera/Follower.cs
@@ -6,34 +6,36 @@ using UnityEngine.Splines;
 
 public class Follower : MonoBehaviour
 {
-    [SerializeField]Transform objectTransform;
-
-    [SerializeField] Camera cam;
-
+    [SerializeField] Transform[] waypoints;
     [SerializeField] Transform playerTransform;
 
-    [SerializeField]CinemachineSmoothPath path;
+    public float speed = 5.0f;
+    public float followDistance = 2.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int currentWaypointIndex = 0;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = objectTransform.position;
+        if (currentWaypointIndex < waypoints.Length)
+        {
+            Transform targetWaypoint = waypoints[currentWaypointIndex];
+            Vector3 directionToWaypoint = (targetWaypoint.position - transform.position).normalized;
 
-        //transform.position = cam.transform.forward;
+  
+            Vector3 playerDirection = playerTransform.forward;
+            Vector3 desiredPosition = playerTransform.position - playerDirection * followDistance;
 
-        Debug.Log(cam.transform.forward);
+            Vector3 movement = Vector3.MoveTowards(transform.position, desiredPosition, speed * Time.deltaTime);
+            transform.position = new Vector3(movement.x, transform.position.y, movement.z);
 
-        
+            if(Vector3.Distance(transform.position, targetWaypoint.position) < 0.1f)
+            {
+                currentWaypointIndex++;
+            }
+        }
 
 
-        if (playerTransform.position.z >= path.m_Waypoints[8].position.z) Debug.Log("TURN");
-
-        //path.m_Waypoints[8].
+        transform.LookAt(playerTransform);
     }
 }

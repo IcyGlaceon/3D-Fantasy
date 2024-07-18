6ceed24be9c1c33e9a12e74db6e6414fa7a6cbb0
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2024-07-18 16:17:09 -0600

    Little Guy Started + Added Some Assets

--- Assets/Scripts/Camera/Follower.cs
+++ Assets/Scripts/Camera/Follower.cs
@@ -2,7 +2,7 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Splines;
+//using UnityEngine.Splines;
 
 public class Follower : MonoBehaviour
 {

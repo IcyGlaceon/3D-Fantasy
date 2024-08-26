5b92f21ebec2842c63c0e0f90cfe4c37759cf0e6
enicewanner <enicewanner@student.neumont.edu>
2024-08-26 10:55:08 -0600

    Level exit

---
+++ Assets/LevelExit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] string levelName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+
+}
